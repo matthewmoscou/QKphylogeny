79a012dffeac15dfe57d69ae2f25aa613a0d2528
matthewmoscou <matthew.moscou@tsl.ac.uk>
2017-09-10 12:06:16 +0100

    Extended parameters for QKphylogeny_alignment_analysis.py

--- QKphylogeny_alignment_analysis.py
+++ QKphylogeny_alignment_analysis.py
@@ -13,6 +13,7 @@ This performs the following:
 """
 
 ## modules
+import commands
 import optparse
 from optparse import OptionParser 
 import sets
@@ -24,13 +25,24 @@ nucleotides = ['A', 'C', 'G', 'T']
 amino_acids = ['A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
 
 
+# functions
+def sequence_selection(sequence, position_index):
+	selected_sequence = ''
+
+	for index in range(len(sequence)):
+		if index in position_index:
+			selected_sequence += sequence[index]
+
+	return selected_sequence
+
+
 ## OptionParser
 # import arguments and options
 usage = "usage: %prog [options]"
 parser = OptionParser(usage=usage)
 parser.add_option("-a", "--alignment", action="store", type="string", dest="alignment", default='', help="Alignment file in Phylip format")
-parser.add_option("-c", "--coverage", action="store", type="float", dest="coverage", default=-1.0, help="Coverage required for inclusion of sequence, coverage above 0.0 but equal to or lower than 1.0")
-parser.add_option("-m", "--missing", action="store", type="float", dest="missing", default=1.0, help="Missing data allowed (0.0 (none) to 1.0 (all))")
+parser.add_option("-b", "--breadth", action="store", type="float", dest="breadth", default=-1.0, help="Breadth coverage required for inclusion of sequence, coverage above 0.0 but equal to or lower than 1.0. Breadth refers to the start and end of a sequence relative to the entire alignment.")
+parser.add_option("-d", "--depth", action="store", type="float", dest="depth", default=-1.0, help="Depth coverage required for inclusion of sequence, coverage above 0.0 but equal to or lower than 1.0. Depth coverage refers to the number of characters with data (i.e. no missing data or gaps).")
 parser.add_option("-n", "--nonredundant", action="store_true", dest="nr", default=False, help="Remove redundant sequences from alignment")
 parser.add_option("-o", "--output", action="store", type="string", dest="output", default='', help="Output file for tree with updated identifiers")
 parser.add_option("-r", "--reduce", action="store_true", dest="reduce", default=False, help="Reduce alignment to polymorphic sites")
@@ -38,7 +50,7 @@ parser.add_option("-t", "--type", action="store", type="string", dest="type", de
 (options, args) = parser.parse_args()
 
 
-## import phylip file
+## Import phylip file
 # check for identical sequence and notify user
 print 'Import Phylip file'
 phylip_input = open(options.alignment, 'r')
@@ -68,12 +80,13 @@ IDs.sort()
 
 phylip_input.close()
 
-print 'Unique sequences :', len(sequence_ID)
-print 'Number of genes  :', len(sets.Set(IDs))
+print 'Unique sequences    :', len(sequence_ID)
+print 'Number of genes     :', len(sets.Set(IDs))
+print 'Length of alignment :', len(sequence_ID.keys()[0])
 print
 
-## assess alignment coverage, requires user-specified coverage of the alignment
-print 'assess alignment coverage'
+## Assess alignment breadth coverage, requires user-specified breadth coverage of the alignment
+print 'Assess alignment breath coverage'
 alignment_length = len(sequence_ID.keys()[0])
 
 truth = False
@@ -110,7 +123,7 @@ for sequence in sequence_ID.keys():
 			if alignment[position_index] in amino_acids:
 				last_sequence = position_index
 
-	if ((float(last_sequence) - float(first_sequence)) / float(alignment_length)) > options.coverage:
+	if ((float(last_sequence) - float(first_sequence)) / float(alignment_length)) > options.breadth:
 		nr_threshold_IDs.append(sequence_ID[sequence][0])
 
 		for gene in sequence_ID[sequence]:
@@ -118,11 +131,11 @@ for sequence in sequence_ID.keys():
 
 
 # if coverage used, limit to genes that meet threshold
-if options.coverage > 0:
+if options.breadth > 0:
 	selected_genes = threshold_IDs
 
-	print '\t' + 'Number of sequences meeting threshold :', len(threshold_IDs)
-	print '\t' + 'Number of non-redundant sequences meeting threshold :', len(nr_threshold_IDs)
+	print '\t' + 'Number of sequences meeting breadth threshold :', len(threshold_IDs)
+	print '\t' + 'Number of non-redundant sequences meeting breadth threshold :', len(nr_threshold_IDs)
 	print
 else:
 	selected_genes = []
@@ -131,12 +144,13 @@ else:
 		for ID in sequence_ID[sequence]:
 			selected_genes.append(ID)
 
-
-# export alignment coverage 
-print 'export alignment coverage'
+# Export alignment coverage 
+print 'Export alignment coverage'
 alignment_coverage_file = open(options.alignment + '_alignment_coverage.txt', 'w')
 	
 alignment_coverage_file.write('position' + '\t' + 'coverage' + '\n')
+
+position_selection = []
 
 for position_index in range(len(alignment)):
 	positive_sites = 0
@@ -145,26 +159,48 @@ for position_index in range(len(alignment)):
 		if options.type == 'nucleotide':
 			if singlesequence in nucleotides:
 				positive_sites += 1
+
 		elif options.type == 'protein':
 			if singlesequence in amino_acids:
 				positive_sites += 1
 	
 	alignment_coverage_file.write(str(position_index + 1) + '\t' + str(float(positive_sites) / float(len(position_sequence[position_index]))) + '\n')
 
+	if (float(positive_sites) / float(len(position_sequence[position_index]))) > options.depth:
+		position_selection.append(position_index)
 
-## reduce evaluated sites
+
+print 'Number of positions meeting depth threshold of', options.depth, 'is', len(position_selection)
+
+
+# Visualization of alignment coverage
+print 'Visualization of alignment coverage'
+
+data_visualization_file = open(options.alignment + '_alignment_coverage.R', 'w')
+data_visualization_file.write('library(ggplot2)' + '\n')
+data_visualization_file.write('\n')
+data_visualization_file.write('alignment = read.table(file="' + options.alignment + '_alignment_coverage.txt", header=T)' + '\n')
+data_visualization_file.write('alignment = data.frame(alignment)' + '\n')
+data_visualization_file.write('\n')
+data_visualization_file.write('png(file="' + options.alignment + '_alignment_coverage.png", height=600, width=600)' + '\n')
+data_visualization_file.write('ggplot(alignment, aes(coverage)) + geom_histogram(binwidth = 0.05) + scale_x_continuous(limits = c(-0.1, 1.1))' + '\n')
+data_visualization_file.write('dev.off()' + '\n')
+data_visualization_file.write('' + '\n')
+data_visualization_file.close()
+
+commands.getstatusoutput('R --vanilla < ' + options.alignment + '_alignment_coverage.R')
+
+
+## Reduce evaluated sites to polymorphic sites
 # check site frequency, evaluate if variation exists
 if options.reduce:
-	print 'reduce evaluated sites'
+	print 'Reduce evaluated sites'
 
 	ID_sequence_polymorphic_sites = {}
 	
 	polymorphic_sites = 0
 	monomorphic_sites = 0
 
-	missing_data_threshold_pass = 0
-	missing_data_threshold_fail = 0
-	
 	for gene in selected_genes:
 		ID_sequence_polymorphic_sites[gene] = ''
 	
@@ -174,27 +210,11 @@ if options.reduce:
 		for gene in selected_genes:
 			evaluated_site_sequence.append(ID_sequence[gene][position_index].upper())
 		
-		if len(sets.Set(evaluated_site_sequence) - sets.Set(['X', 'N'])) > 1:
+		if len(sets.Set(evaluated_site_sequence) - sets.Set(['X', 'N', '-', '?', 'O'])) > 1:
 			polymorphic_sites += 1
 	
-			missing_data = 0.0
-
-			if options.type == 'nucleotide':
-				for gene in selected_genes:
-					if ID_sequence[gene][position_index].upper() in ['N', 'O', 'X', '?', '-']:	
-						missing_data += 1
-			elif options.type == 'protein':
-				for gene in selected_genes:
-					if ID_sequence[gene][position_index].upper() in ['X', '?', '­']:
-						missing_data += 1
-
-			if ((float(len(evaluated_site_sequence)) - missing_data) / float(len(evaluated_site_sequence))) >= (1.0 - options.missing):
-				missing_data_threshold_pass += 1
-
-				for gene in selected_genes:
-					ID_sequence_polymorphic_sites[gene] += ID_sequence[gene][position_index].upper()
-			else:
-				missing_data_threshold_fail += 1
+			for gene in selected_genes:
+				ID_sequence_polymorphic_sites[gene] += ID_sequence[gene][position_index].upper()
 		else:
 			monomorphic_sites += 1
 	
@@ -210,10 +230,6 @@ if options.reduce:
 	print '\t' + 'Polymorphic:', polymorphic_sites
 	print '\t' + 'Monomorphic:', monomorphic_sites
 	
-	print 'Sites that passed missing data threshold of ' + str(options.missing * 100.0) + '%:'
-	print '\t' + 'Pass:', missing_data_threshold_pass
-	print '\t' + 'Fail:', missing_data_threshold_fail
-
 	selected_genes = []
 	
 	for sequence in sequence_ID_polymorphic_sites.keys():
@@ -238,17 +254,17 @@ print 'output files'
 phylip_digital = open(options.output, 'w')
 
 print 'Genotypes in alignment:', len(sequence_ID_polymorphic_sites.keys())
-print 'Length of alignment:   ', len(sequence_ID_polymorphic_sites.keys()[0])
+print 'Length of alignment:   ', len(sequence_selection(sequence_ID_polymorphic_sites.keys()[0], position_selection))
 
 # export header for phylip file
 
 # export identifier and sequence, final check of gene identifier length
 if options.nr:
-	phylip_digital.write(' ' + str(len(selected_genes)) + ' ' + str(len(ID_sequence[selected_genes[0]])) + '\n')
+	phylip_digital.write(' ' + str(len(selected_genes)) + ' ' + str(len(sequence_selection(ID_sequence[selected_genes[0]], position_selection))) + '\n')
 	phylip_digital_crosslist = open(options.output + '_crosslist.txt', 'w')
 	
 	for sequence in sequence_ID_polymorphic_sites.keys():
-		phylip_digital.write(sequence_ID_polymorphic_sites[sequence][0] + '   ' + sequence + '\n')
+		phylip_digital.write(sequence_ID_polymorphic_sites[sequence][0] + '   ' + sequence_selection(sequence, position_selection) + '\n')
 
 		phylip_digital_crosslist.write(sequence_ID_polymorphic_sites[sequence][0])
 
@@ -260,10 +276,10 @@ if options.nr:
 	
 	phylip_digital_crosslist.close()
 else:
-	phylip_digital.write(' ' + str(len(selected_genes)) + ' ' + str(len(ID_sequence_polymorphic_sites[selected_genes[0]])) + '\n')
+	phylip_digital.write(' ' + str(len(selected_genes)) + ' ' + str(len(sequence_selection(ID_sequence_polymorphic_sites[selected_genes[0]], position_selection))) + '\n')
 
 	for gene in selected_genes:
-		phylip_digital.write(gene + '   ' + ID_sequence_polymorphic_sites[gene] + '\n')
+		phylip_digital.write(gene + '   ' + sequence_selection(ID_sequence_polymorphic_sites[gene], position_selection) + '\n')
 
 
 phylip_digital.close()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A set of scripts for phylogenetic tree assessment and editing.
 
 <i>QKphylogeny_rename_nodes.py</i> will convert genes/protein identifiers in a phylogenetic tree based on a tab-delimited translation table.
 
-<i>QKphylogeny_alignment_analysis.py</i> assesses the quality of a multiple sequence alignment, removes redundancy, and reduces alignment to polymorphic sites.
+<i>QKphylogeny_alignment_analysis.py</i> assesses the quality of a multiple sequence alignment. Parameters exist for the removal of redundant sequences, reduction of the alignment based on breadth and depth coverage, and reduction of alignment to polymorphic sites.
 
 ## Examples
 Export the proteins in a phylogenetic tree in Newick format. In this case, using the neighbor joining tree for thioredoxins in <i>Arabidopsis thaliana</i>.
@@ -17,4 +17,9 @@ python QKphylogeny_nodelabels.py -t examples/AT_TRX_phylogenetic_tree_NJ.newick 
 Convert the protein identifiers from one format to another. In this case, convert long gene format (i.e. AT1G03680.1 to AtTRXm1).
 ```bash
 python QKphylogeny_rename_nodes.py -t examples/AT_TRX_phylogenetic_tree_NJ.newick -l examples/AT_TRX_abbreviations.txt -o AT_TRX_phylogenetic_tree_NJ_renamed.newick
+```
+
+Restrict the nucleotide alignment to evaluated sites that have more than 80% of data present
+```bash
+python QKphylogeny_alignment_analysis.py -a test.phy -d 0.2 -t nucleotide -o test_missing20.phy
 ```
